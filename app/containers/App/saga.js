a86191164cb1170ce4e7006549658543c575d713
fernando <fvalarinon@gmail.com>
2019-01-28 09:30:10 +0100

    linter now works?

--- app/containers/App/saga.js
+++ app/containers/App/saga.js
@@ -2,13 +2,30 @@
  * Gets the repositories of the user from Github
  */
 
-import { all, call, put, fork, takeLatest, race, cancelled, take } from 'redux-saga/effects'
-import { push } from 'connected-react-router'
-import { LOAD_USER_STATUS, EVENT_POLLING_START, EVENT_POLLING_STOP } from 'containers/App/constants'
-import { userLoaded, userLoadingError, startEventPolling, stopEventPolling } from 'containers/App/actions'
-import { API_HOSTNAME } from 'utils/constants'
-import { get } from 'utils/request'
-import auth from 'utils/auth'
+import {
+  all,
+  call,
+  put,
+  fork,
+  race,
+  cancelled,
+  take,
+} from 'redux-saga/effects';
+import { push } from 'connected-react-router';
+import {
+  LOAD_USER_STATUS,
+  EVENT_POLLING_START,
+  EVENT_POLLING_STOP,
+} from 'containers/App/constants';
+import {
+  userLoaded,
+  userLoadingError,
+  startEventPolling,
+  stopEventPolling,
+} from 'containers/App/actions';
+import { API_HOSTNAME } from 'utils/constants';
+import { get } from 'utils/request';
+import auth from 'utils/auth';
 
 /**
  * Github repos request/response handler
@@ -18,50 +35,47 @@ export function* getUserData() {
   const token = auth.getToken();
   const requestURL = `${API_HOSTNAME}players/me`;
   if (!token) {
-    yield put(userLoaded(false, false, {}))
-  }
-  else {
+    yield put(userLoaded(false, false, {}));
+  } else {
     try {
-      const response = yield call(get, requestURL)
-      yield put(userLoaded(true, response.data))
-      yield put(startEventPolling())
+      const response = yield call(get, requestURL);
+      yield put(userLoaded(true, response.data));
+      yield put(startEventPolling());
     } catch (err) {
-      yield put(userLoadingError(err))
+      yield put(userLoadingError(err));
     }
   }
 }
 
-function * pollForMatches() {
+function* pollForMatches() {
   let response;
   while (true) {
     try {
       const requestURL = `${API_HOSTNAME}games/listen`;
       response = yield call(get, requestURL);
-      console.log(response);
       yield put(push(`/match/${response.data.id}`));
-      yield put(stopEventPolling())
+      yield put(stopEventPolling());
       break;
     } catch (err) {
       if (yield cancelled()) {
-        console.log("Event poll loop cancelled");
         break;
       }
     }
   }
 }
 
-function * watchUserData() {
-  yield takeLatest(LOAD_USER_STATUS, getUserData);
+function* watchUserData() {
+  while (true) {
+    yield take(LOAD_USER_STATUS);
+    yield fork(getUserData);
+  }
 }
 
-function * watchPollData() {
-  while(true) {
+function* watchPollData() {
+  while (true) {
     yield take(EVENT_POLLING_START);
-    
-    yield race([
-      take(EVENT_POLLING_STOP),
-      fork(pollForMatches),
-    ]);
+
+    yield race([take(EVENT_POLLING_STOP), fork(pollForMatches)]);
   }
 }
 
@@ -70,8 +84,5 @@ function * watchPollData() {
  */
 export default function* userData() {
   yield fork(getUserData);
-  yield all([
-    watchPollData(),
-    watchUserData()
-  ])
+  yield all([watchPollData(), watchUserData()]);
 }
