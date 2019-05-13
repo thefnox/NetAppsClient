eec43ef9880397a243f198bba95b90343d1e6b1d
fnox <thefnox@gmail.com>
2019-05-13 23:03:58 +0200

    Updated API hostname

--- app/utils/constants.js
+++ app/utils/constants.js
@@ -1,4 +1,4 @@
 export const RESTART_ON_REMOUNT = '@@saga-injector/restart-on-remount';
 export const DAEMON = '@@saga-injector/daemon';
 export const ONCE_TILL_UNMOUNT = '@@saga-injector/once-till-unmount';
-export const API_HOSTNAME = 'http://netappsmm-env.8ryvux5at3.eu-north-1.elasticbeanstalk.com/';
+export const API_HOSTNAME = 'http://netappsmm.eu-north-1.elasticbeanstalk.com/';
